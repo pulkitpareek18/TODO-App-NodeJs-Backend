fca88138584b0971b89ffc0651124ac9750e37f2
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2024-05-17 16:13:45 +0530

    used Errorhandler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,14 @@
+import { User } from "../models/user.js";
 import jwt from "jsonwebtoken";
-import {User} from "../models/user.js";
 
 export const isAuthenticated = async (req, res, next) => {
-    const {token} = req.cookies;
+  const { token } = req.cookies;
 
-    if(!token){
-        return res.status(404).json({
-            success: false,
-            message: "Login First"
-        });
-    }
+if (!token)
+    return  next(new ErrorHandler("Login First",404))
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = await User.findById(decoded._id);
-    next();
-}
+  req.user = await User.findById(decoded._id);
+  next();
+};
